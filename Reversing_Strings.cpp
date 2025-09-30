65216ea8fd748d09662a82c4936da5c24f3b76b7
Yashvesh20 <yashvesh20@gmail.com>
2025-09-30 21:03:07 +0530

    Create Reversing_Strings.cpp

---
+++ Reversing_Strings.cpp
@@ -0,0 +1,22 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+void rev(char*str){
+   if (*str)
+   {
+       rev(str+1);
+       cout<<("%c", *str);
+   }
+}
+ 
+int main(){
+   char ch[50];
+   cout<<"Enter a string: ";
+   cin>>ch;
+   rev(ch);
+}
+
+output:
+Enter a string: yashvesh
+hesvhsay
+
