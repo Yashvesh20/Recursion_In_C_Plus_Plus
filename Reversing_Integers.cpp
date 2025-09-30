91802194730dcbb47594750b85d090bf8f68a44d
Yashvesh20 <yashvesh20@gmail.com>
2025-09-30 21:01:52 +0530

    Create Reversing_Integers.cpp

---
+++ Reversing_Integers.cpp
@@ -0,0 +1,21 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+
+void revint(int a){
+    if(a>0)
+    {
+        cout<<(a%10);
+        revint(a/10);
+    }
+}
+    
+int main(){
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+    revint(a);
+} 
+output:
+Enter the number: 12
+21
